71dafabf589093966de24938e790303d7d9550fc
Yogandovi01 <31580579+Yogandovi01@users.noreply.github.com>
2019-04-12 21:21:10 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
-"imei" => "xxxxxxxxxxxxx",
-"sign" => "xxxxxxxxxxxxx",
-"token" => "xxxxxxxxxxxxx",
-"uuid" => "xxxxxxxxxxxxx"
+"imei" => "626389146671566",
+"sign" => "5583ecad0752498db27a13635ccc0d74f0eae716",
+"token" => "7d86f7dfc7fa2dd36525130aef5b54f2",
+"uuid" => "b88fae4c9684e7ec"
 ];
